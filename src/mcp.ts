94cf30b7267016328559a8795a52f21b049dc743
sanchezg7 <gsanc.tech@pm.me>
2025-11-14 12:46:49 -0500

    wip: adding zod

--- src/mcp.ts
+++ src/mcp.ts
@@ -1,6 +1,18 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { z } from "zod";
 
 const server = new McpServer({
     name: 'workshop-mcp-server',
     version: '1.0.0'
 });
+
+server.registerTool('multiply', {
+    title: 'Multiply',
+    description: 'Multiply two numbers',
+    inputSchema: { a: z.number(), b: z.number() },
+    outputSchema: { result: z.number() }
+}, async ({ a, b}) => {
+    return {
+        content: [{type: "text", "text": String(a * b)}]
+    }
+})
